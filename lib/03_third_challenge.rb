2777901484212d312b4ded826fd5992ab038cdc5
Jacob Kagon <jacob@kagon.net>
2020-09-15 18:49:50 +0000

    Automatically backed up by Learn

--- lib/03_third_challenge.rb
+++ lib/03_third_challenge.rb
@@ -11,7 +11,7 @@ def third_challenge
         },
       :hero => {
         :name => "Romeo"
-        :age => "15"
+        :age => "14"
         :status => "alive"
         },
       :hero_friends => []
